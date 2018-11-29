2d59e8aaea0988349920fc51d869ed711f5a61fe
jorcebs <jor.cebs@gmail.com>
2018-11-29 10:16:53 -0600

    Badges page finished
    - style improvements
    -badges page finished

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
 $(document).ready(function () {
     $('.tabs').tabs();
+    $('.modal').modal({
+        startingTop: '4%',
+        endingTop: '20%'
+    });
     $('.sidenav').sidenav();
     $('.collapsible').collapsible();
     $('.dropdown-trigger').dropdown({
         hover: true,
         coverTrigger: false
     });
-    $('.tooltipped').tooltip({
-        margin: 50,
-        inDuration: 0,
-        outDuration: 0,
+    $('.tooltipped-lock').tooltip({
+        position: 'top'
+    });
+    $('.tooltipped-badge-page').tooltip({
+        position: 'top',
+        margin: 105,
         enterDelay: 0,
         exitDelay: 0
     });
-    $('.tooltipped-lock').tooltip({
-        position: 'top'
+    $('.tooltipped-activity-badge').tooltip({
+        position: 'top',
+        margin: 50,
+        enterDelay: 0,
+        exitDelay: 0
     });
 });
