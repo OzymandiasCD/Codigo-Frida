1b8fbbb78955dbff52d9c22a094c8b64c7d57fb6
jorcebs <jor.cebs@gmail.com>
2018-11-30 13:42:17 -0600

    .

--- includes/header.php
+++ includes/header.php
@@ -7,49 +7,71 @@
 <html lang='en'>
 
 <head>
-
+    <title>
+        <?php echo $title ?>
+    </title>
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-
-    <!-- Echoes title set in every page -->
-    <title>
-        <?php echo $title;?>
-    </title>
-
-    <!-- Custom CSS -->
-    <link rel='stylesheet' href='css/reset.css'>
+    <link rel='shortcut icon' href='img/icon.png' type='image/x-icon'>
     <link rel='stylesheet' href='css/style.css'>
-
-    <!-- Materialize CSS and JavaScript -->
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Quicksand:400,500,700'>
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css'>
-    <script src='https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js'></script>
-
-    <!-- jQuery -->
+    <link rel='stylesheet' href='https://fonts.googleapis.com/icon?family=Material+Icons'>
     <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
-    <script>
-        window.jQuery || document.write('<script src="js/jquery-3.3.1.min.js"><\/script>');
-    </script>
-
-    <!-- Custom JavaScript -->
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js'></script>
     <script src='js/script.js'></script>
 </head>
 
 <body>
-    <div id='container'>
-        <header>
-            <img src='img/santa.png' width='180' height='200' alt='Santa waving'>
-            <img src='img/raindeer.png' width='180' height='200' alt='Reindeer waving'>
-            <h1>My Grown-up Christmas List</h1>
-            <h2>Checkin' it Twice</h2>
-        </header>
 
-        <nav>
-            <ul>
-                <?php if(isset($_SESSION['userid'])):?>
-                <li <?php if($page=='index.php' ):?>class='active'
-                    <?php endif ?>>
-                    <a href='index.php'>Home</a>
-                </li>
-                <?php endif?>
+    <nav class='pink darken-1'>
+        <div class='nav-wrapper container'>
+            <a class='brand-logo' href='course.html'><img src='img/logo-white.png'></a>
+            <a class='sidenav-trigger' data-target='sidenav'><i class='white-text material-icons'>menu</i></a>
+            <ul class='right hide-on-med-and-down'>
+                <li><a class='white-text' href='course.html'>Curso</a></li>
+                <li><a class='white-text' href='badges.html'>Mis insignias</a></li>
+                <li><a class='white-text' href='team.html'>Mi equipo</a></li>
+                <li><a class='white-text' href='mentors.html'>Mentores</a></li>
+                <li><a class='white-text' href='fridas.html'>Fridas</a></li>
+                <li><a class='white-text dropdown-trigger valign-wrapper' href='profile.html' data-target='profile-dropdown'>
+                        Jorge Ceballos
+                        <img class='circle user-nav right' src='img/user.jpg'>
+                    </a></li>
             </ul>
-        </nav>
+        </div>
+    </nav>
+
+    <ul class='dropdown-content' id='profile-dropdown'>
+        <li class='pink darken-1'><a class='white-text' href='profile.html'>Mi perfil</a></li>
+        <li class='pink darken-1'><a class='white-text' href='index.html'>Cerrar sesión</a></li>
+    </ul>
+
+    <ul class='sidenav' id='sidenav'>
+        <li>
+            <div class='user-view'>
+                <div class='background'>
+                    <img src='img/sidenav-background.jpg'>
+                </div>
+                <a href='profile.html'>
+                    <img class='circle user-sidenav' src='img/user.jpg'>
+                    <span class='white-text name'>Jorge Ceballos</span>
+                    <span class='white-text email'>jor.cebs@gmail.com</span>
+                </a>
+            </div>
+        </li>
+        <li><a class='black-text' href='course.html'><i class='material-icons'>widgets</i>Curso</a></li>
+        <li><a class='black-text' href='badges.html'><i class='material-icons'>grade</i>Mis insignias</a></li>
+        <li>
+            <div class='divider'></div>
+        </li>
+        <li><a class='black-text' href='team.html'><i class='material-icons'>group</i>Mi equipo</a></li>
+        <li><a class='black-text' href='mentors.html'><i class='material-icons'>local_cafe</i>Mentores</a></li>
+        <li><a class='black-text' href='fridas.html'><i class='material-icons'>favorite</i>Fridas</a></li>
+        <li>
+            <div class='divider'></div>
+        </li>
+        <li><a class='black-text' href='index.html'><i class='material-icons'>exit_to_app</i>Cerrar sesión</a></li>
+    </ul>
+
+    <main class='container'>
