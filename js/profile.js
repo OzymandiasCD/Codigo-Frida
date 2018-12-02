3c3b452f81f5a9d767dbedf62a3230840384b2dd
jorcebs <jor.cebs@gmail.com>
2018-12-02 14:25:56 -0600

    Profile improving

---
+++ js/profile.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    // Materialize tooltip initialization
+    $('.small-badge').tooltip({
+        position: 'top',
+        enterDelay: 0,
+        exitDelay: 0,
+        margin: 0
+    });
+    
+});
