39455fb501291941a2c786f1df81ee7cdbff991a
Daniel Herrera <carlos_danielherr@hotmail.com>
2018-12-04 02:50:13 -0600

    Demen un Red Bull

--- js/activity.js
+++ js/activity.js
@@ -1,11 +1,38 @@
-$(document).ready(function () {
+$(document).ready(function() {
+    $.getScript('js/session.js', function() {
+        checkCurrentSession(function(response) {
+            // There is no current session ongoing on this computer
+            // return to the index.html page
+            if (response.data === null) {
+                window.open('index.html', '_self');
+            } else {
+                // Set a handler for the logout button
+                $('.logout').click(function() {
+                    closeSession(function(response) {});
+                });
 
-    // Materialize tooltip initialization
-    $('.tooltipped').tooltip({
-        position: 'top',
-        margin: 48,
-        enterDelay: 0,
-        exitDelay: 0
+                // Load the basic user info
+                $.post('php/user_info.php', function(response) {
+                    let name = response.data.name.split(" ")[0] + " " + response.data.last_name.split(" ")[0];
+                    $('.user-min-name').text(name);
+                    $('.user-min-email').text(response.data.email);
+                    $('.user-min-image').attr('src', response.data.image_path);
+
+                    M.updateTextFields();
+                }, 'json');
+
+                $.post('php/activity.php', function(response) {
+                    console.log(response);
+                });
+
+                // Materialize tooltip initialization
+                $('.tooltipped').tooltip({
+                    position: 'top',
+                    margin: 48,
+                    enterDelay: 0,
+                    exitDelay: 0
+                });
+            }
+        });
     });
-
 });
